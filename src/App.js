1ce0e785086091d006d40f6e255c60df33cd037b
Alice Kim <aliceykim0828@gmail.com>
2021-09-24 20:27:22 -0700

    Implement random pictionary topic generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react'
+import { sample } from 'lodash'
+import words from './constants'
+import refresh from './refresh.png'
 import './App.css';
 
 function App() {
+  const [word, setWord] = useState(sample(words))
+
+  function Refresh() {
+    return <img src={refresh} alt="refresh" className="icon" onClick={() => setWord(sample(words))} />
+  }
+
+  function Main({ word: { category, value } }) {
+    return <div className="main">
+      <p className="category">Category: {category}</p>
+      <p className="value">{value}</p>
+      <Refresh />
+    </div>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {<Main word={word} />}
     </div>
   );
 }
